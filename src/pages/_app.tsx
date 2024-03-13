2b5880efab99709233d04cde4e77b1e97dd2db87
noorimanr@gmail.com <noorimanr@gmail.com>
2024-03-13 14:34:43 +0800

    navbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import Navbar from "@/components/layout/Navbar";
+import Navbar from "@/components/layout/navbar";
 import "@/styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
