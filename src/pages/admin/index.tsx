07f2a825c6fe75eb172d998274388a234b08f08c
noorimanr@gmail.com <noorimanr@gmail.com>
2024-03-22 23:18:05 +0800

    comit

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -3,8 +3,8 @@ import AdminLayout from "@/components/layout/adminlayout";
 const AdminPage = () => {
   return (
     <AdminLayout>
-      <div className="">
-        <div className="">Admin Page</div>;
+      <div>
+        <div className="">Admin Page</div>
       </div>
     </AdminLayout>
   );
--- src/pages/admin/product.tsx
+++ src/pages/admin/product.tsx
@@ -3,7 +3,7 @@ import AdminLayout from "@/components/layout/adminlayout";
 const AdminProductPage = () => {
   return (
     <AdminLayout>
-      <div className="">
+      <div>
         <div className="">Admin Product Page</div>;
       </div>
     </AdminLayout>
